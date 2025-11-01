d185b0d29b0f5cb0cb9b5ceec75327cb17aebef7
Arktik1st <160301926+Arktik1st@users.noreply.github.com>
2025-11-01 23:50:41 +0300

    Продолжаю делать 2 лабу
    Я пропустил некоторые пункты в задании, доделаю позже

--- lab002_8.cpp
+++ lab002_8.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
 using namespace std;
-
+//я в задании не увидел некоторые требования, переделаю их позже
 class Rational{
     int a, b;
     
+    public:
+    Rational(int aa = 1, int bb = 1){
+        a = aa;
+        b = bb;
+    }
+    
+    void drob_Change(int c, int d){
+        a = c;
+        b = d;
+    }
+    
+    void vivod(){
+        cout
+    }
     void add(int c, int d){
-        this a += c;
-        this b *= d;
+        a += c;
+        b *= d;
     }
     
     void sub(int c, int d){
-        this a -= c;
-        this b *= d;
+        a -= c;
+        b *= d;
     }
     
     void mul(int c, int d){
-        this a *= c;
-        this b *= d;
+        a *= c;
+        b *= d;
     }
     
     void div(int c, int d){
-        this a *= d;
-        this b *= c;
+        a *= d;
+        b *= c;
+    }
+
+    //общий делитель по алгоритму евклида
+    int del(){
+        int c = a, d = b;
+        while(c>0 && d>0){
+            if(c >= d) c %= d;
+            else d %= c;
+        }
+        if(c > d) return c;
+        else return d;
     }
     
     bool equal(Rational c1, Rational c2){
-        
+        int d1 = c1.del();
+        int d2 = c2.del();
+        return true;
     }
 };
 
 int main() {
-    
+    Rational d1, d2;
     return 0;
 }
