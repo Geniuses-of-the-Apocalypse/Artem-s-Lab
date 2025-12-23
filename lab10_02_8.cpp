30bb9b6490042cf135c5984aaa248d6681fb1d3d
Arktik1st <160301926+Arktik1st@users.noreply.github.com>
2025-12-23 10:51:13 +0300

    Update lab10_02_8.cpp

--- lab10_02_8.cpp
+++ lab10_02_8.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 //вспомогательная структура для хранения значений
 template<typename... Ts>
@@ -16,15 +17,15 @@ struct ReversedTuple<> {
 template<typename T, typename... Ts>
 struct ReversedTuple<T, Ts...> {
     static auto reverse(T&& first, Ts&&... rest) {
-        return [first = std::forward<T>(first), 
-                rest_tuple = ReversedTuple<Ts...>::reverse(std::forward<Ts>(rest)...)]() mutable {
+        return [first = forward<T>(first), 
+                rest_tuple = ReversedTuple<Ts...>::reverse(forward<Ts>(rest)...)]() mutable {
             rest_tuple();
             return first;
         };
     }
 };
 
-//основная функция для C++17
+//основная функция 
 template<typename... Args>
 auto reverseArgs17(Args&&... args) {
     //fold expression для создания лямбды
@@ -33,16 +34,16 @@ auto reverseArgs17(Args&&... args) {
     };
     
     //возвращаем функцию, которая будет вызываться рекурсивно
-    return ReversedTuple<Args...>::reverse(std::forward<Args>(args)...);
+    return ReversedTuple<Args...>::reverse(forward<Args>(args)...);
 }
 
 int main() {
-    auto result1 = reverseArgs(1);
-    cout << "reverseArgs(1) = " << result1 << endl;
-    auto result2 = reverseArgs(1, 2);
-    cout << "reverseArgs(1, 2) = " << result2 << endl;
-    auto result3 = reverseArgs(1, 2, 3);
-    cout << "reverseArgs(1, 2, 3) = " << result3 << endl;
-    auto result4 = reverseArgs(1, 2, 3, 4);
+    auto result1 = reverseArgs17(1);
+    cout << "reverseArgs17(1) = " << result1 << endl;
+    auto result2 = reverseArgs17(1, 2);
+    cout << "reverseArgs17(1, 2) = " << result2 << endl;
+    auto result3 = reverseArgs17(1, 2, 3);
+    cout << "reverseArgs17(1, 2, 3) = " << result3 << endl;
+    auto result4 = reverseArgs17(1, 2, 3, 4);
     cout << "reverseArgs(1, 2, 3, 4) = " << result4 << endl;
 }
